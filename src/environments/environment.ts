d8a2facd8d17cce6e18effc9eadfdeff8221b64b
Kaigonia <ktt45678@gmail.com>
2020-06-21 21:06:50 +0700

    Improved register page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  clientId: '515hwrFpeLvGeVo2SvSe',
+  clientSecret: 'GkehqK7jqutGisHQV5B59IBsQeI3Kt16lpe3uKzTn1GIfbKZcGTvL9iLpeK86jA9sp9SFXFQX7yszpflqkCsJHa33wb8SUEDq1HSCadLldp1VxP8lmi'
 };
 
 /*
