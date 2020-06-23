d3e50d0ca83953cbe9be252e2a4dc3aa03a33f49
Kaigonia <ktt45678@gmail.com>
2020-06-23 14:06:59 +0700

    Multiple router outlets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MatheaderComponent } from './shared/matheader/matheader.component';
+import { MatfooterComponent } from './shared/matfooter/matfooter.component';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -12,7 +17,11 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
+  { path: '', component: HeaderComponent, outlet: "header" },
+  { path: '', component: FooterComponent, outlet: "footer" },
+  { path: 'matheader', component: MatheaderComponent, outlet: "header" },
+  { path: 'matfooter', component: MatfooterComponent, outlet: "footer" }
 ];
 
 @NgModule({
