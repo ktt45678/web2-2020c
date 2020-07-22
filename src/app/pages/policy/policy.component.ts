a097a4bd3501ad370adcd8b0ae5b7e360970d3a3
ntlooooong <ntlong.17ck1@gmail.com>
2020-07-22 16:48:36 +0700

    add sidenav , fix align problems

--- src/app/pages/policy/policy.component.ts
+++ src/app/pages/policy/policy.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-policy',
   templateUrl: './policy.component.html',
   styleUrls: ['./policy.component.scss']
 })
-export class PolicyComponent implements OnInit {
+// export class PolicyComponent implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit(): void {
+//   ngOnInit(): void {
+//   }
+
+// }
+
+
+export class PolicyComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
