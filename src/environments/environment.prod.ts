647be3ebe7a59d3a6abac583a54b403a134721d7
Kaigonia <ktt45678@gmail.com>
2020-07-31 16:51:35 +0700

    Minor change for production server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://whitefoobank.azurewebsites.net',
   clientId: '515hwrFpeLvGeVo2SvSe',
   clientSecret: 'GkehqK7jqutGisHQV5B59IBsQeI3Kt16lpe3uKzTn1GIfbKZcGTvL9iLpeK86jA9sp9SFXFQX7yszpflqkCsJHa33wb8SUEDq1HSCadLldp1VxP8lmi',
   reCaptchaKey: '6LfReqcZAAAAAA2CC5SjI_bqNSNOJPwrOjjdAHHE'
