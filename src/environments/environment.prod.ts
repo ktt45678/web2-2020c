fb70b2d20293ac17e2b2f35b739453b130f45d04
Kaigonia <ktt45678@gmail.com>
2020-09-05 16:42:51 +0700

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://whitefoobank.azurewebsites.net',
+  apiUrl: 'https://whitefoobank.herokuapp.com',
   clientId: '515hwrFpeLvGeVo2SvSe',
   clientSecret: 'GkehqK7jqutGisHQV5B59IBsQeI3Kt16lpe3uKzTn1GIfbKZcGTvL9iLpeK86jA9sp9SFXFQX7yszpflqkCsJHa33wb8SUEDq1HSCadLldp1VxP8lmi',
   reCaptchaKey: '6LfReqcZAAAAAA2CC5SjI_bqNSNOJPwrOjjdAHHE'
